f2dd1025e6c6b9446e5807f4deebbea45fd3ea4d
Bendrik <oskar.bendrik@gmail.com>
2020-09-15 15:48:30 +0200

    fixat lite med start

--- res/script.js
+++ res/script.js
@@ -3,10 +3,19 @@ $(document).ready(function() {
 
     $('main').load('start.html');
 
-
     $('#home').click(function (event) {
-    event.preventDefault();
-    $('main').load('start.html');
+        event.preventDefault();
+        $('main').load('start.html');
+    });
+
+    $('#portfolio').click(function (event) {
+        event.preventDefault();
+        $('main').load('portfolio.html');
+    });
+
+    $('#aboutUs').click(function (event) {
+        event.preventDefault();
+        $('main').load('aboutUs.html');
     });
 
 });
