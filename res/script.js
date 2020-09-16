8833c816ed5f94abdec80e0f17abfd8c66ebea50
pownas <pownas@outlook.com>
2020-09-16 10:07:43 +0200

    Byter URL till filerna

--- res/script.js
+++ res/script.js
@@ -1,21 +1,21 @@
 /* This JavaScript need jQuery to run */
 $(document).ready(function() {
 
-    $('#page-content').load('../pages/start.html');
+    $('#page-content').load('./pages/start.html');
 
     $('#home').click(function (event) {
         event.preventDefault();
-        $('#page-content').load('../pages/start.html');
+        $('#page-content').load('./pages/start.html');
     });
 
     $('#portfolio').click(function (event) {
         event.preventDefault();
-        $('#page-content').load('../pages/portfolio.html');
+        $('#page-content').load('./pages/portfolio.html');
     });
 
     $('#about').click(function (event) {
         event.preventDefault();
-        $('#page-content').load('../pages/about.html');
+        $('#page-content').load('./pages/about.html');
     });
 
 });
