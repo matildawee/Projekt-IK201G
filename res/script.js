baa717a237dce52add20aae6f60e861a90e4440c
Bendrik <oskar.bendrik@gmail.com>
2020-09-17 10:22:55 +0200

    Update script.js

--- res/script.js
+++ res/script.js
@@ -3,20 +3,12 @@ $(document).ready(function() {
 
     $('main').load('start.html');
 
+
     $('#home').click(function (event) {
-        event.preventDefault();
-        $('main').load('start.html');
-    });
-
-    $('#portfolio').click(function (event) {
-        event.preventDefault();
-        $('main').load('portfolio.html');
-    });
-
-    $('#aboutUs').click(function (event) {
-        event.preventDefault();
-        $('main').load('aboutUs.html');
+    event.preventDefault();
+    $('main').load('start.html');
     });
 
 });
 
+//aaaaa
