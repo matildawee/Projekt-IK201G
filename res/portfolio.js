cb924bad0163dcf7c0fe96de8bf87923034e7748
matildawe <63040887+matildawe@users.noreply.github.com>
2020-09-23 15:54:43 +0200

    testing projects

--- res/portfolio.js
+++ res/portfolio.js
@@ -22,10 +22,10 @@ $(document).ready(function() {
 
     $.each(projects, function (i, portfolio) {          
         var aProject = 
-            '<div class="subPortfolio">' + 
-            '<img class="project-img" src="' + portfolio.image + '" title="Project" alt="Project">' +
-                '<h1>' + portfolio.title + '</h1>' +
-                '<p>' + portfolio.description + '</p>' +
+            '<div class="subPortfolio" id="' + portfolio.projectId + '">' + 
+            '<img class="project-img" id="' + portfolio.projectId + '"src="' + portfolio.image + '" title="Project" alt="Project">' +
+                '<h1 id="' + portfolio.projectId + '">' + portfolio.title + '</h1>' +
+                '<p id="' + portfolio.projectId + '">' + portfolio.description + '</p>' +
             '</div>';
         
         //skapar en div per projekt
@@ -58,8 +58,6 @@ $(document).ready(function() {
         divList.splice(divList.length-1,1); 
         //raderar en sista tom div som skapas när antalet projekt är delbart med 4
     }
-
-    console.log(divList);
     $('#portfolio-box').html(divList[0]);
     
     };
@@ -91,5 +89,66 @@ $(document).ready(function() {
         $('#portfolio-box').html(divList[showIndex]);
         
     });
-
 });
+
+//TESTING!!!!!!
+
+$('.portfolioMain').find(".subPortfolio").click(function (event) {
+    console.log("knapp fungerar");
+    event.preventDefault();
+    var id = (event.target.id);
+
+    $(".projectdiv").show();
+    $(".project-content").show();
+
+    $("body").css({"overflow": "hidden"});
+    
+    // Loads persons from about-data.json
+    $.getJSON(
+        'res/portfolio-data.json',
+        function (data) {
+            getProject(data.projects, id);
+        }
+    );
+});
+
+function getProject(projects, id) {
+    $.each(projects, function (ind, project) {
+            if (project.id == id)
+            {
+                var projectSquare = $(
+                    '<div class="project-content" id="projectId' + ind + '">' + 
+                        '<div class="project-content-left">' +
+                            '<span class="fas fa-times" id="close-portfolio"></span>' +
+                            '<img class="project-image" src="' + projects.image + '" title="developer" alt="developer">' +                                
+                        '</div>' +
+                        '<div class="project-content-right">' + 
+                            '<h2> ' + projects.title + '</h2>' + '<hr/>' +
+                            '<p>' + projects.description +'</p>' + 
+                        '</div>' +
+                            '<br/>' +
+                            '<p>' + projects.date + '</p>' +                            
+                    '</div>'
+                );
+            $('.projectdiv').html(projectSquare);  
+            };
+    });
+};
+
+// $(".about-personal").on("click", "#close-person", function(event){
+//     event.preventDefault();
+//     $('.persondiv').html('');
+//     $(".person-content").hide();
+//     $(".persondiv").hide(); 
+//     $("body").css({"overflow": "scroll"});
+// });
+
+// $(".about-personal").on("click", "#thePersondiv", function(event){
+//     if(event.target.id=="thePersondiv"){
+//         event.preventDefault();
+//         $('.persondiv').html('');
+//         $(".person-content").hide();
+//         $(".persondiv").hide();
+//         $("body").css({"overflow": "scroll"});
+//     }; 
+//});
